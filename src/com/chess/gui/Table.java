0971ca88091d2b8c81965c73aee01a732100555b
Mert Karakas <mertkarakas1997@gmail.com>
2016-09-21 19:35:02 -0400

    updated code

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -5,6 +5,8 @@ import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.MoveTransition;
+import com.google.common.collect.Lists;
+import com.sun.istack.internal.Interned;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -35,6 +37,7 @@ public class Table {
     private Tile sourceTile;
     private Tile destinationTile;
     private Piece humanMovedPiece;
+    private BoardDirection boardDirection;
 
     // dimension reference #31 4:17
     private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
@@ -54,12 +57,14 @@ public class Table {
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
         this.chessBoard = Board.createStandardBoard();
         this.boardPanel = new BoardPanel();
+        this.boardDirection = BoardDirection.NORMAL;
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
     private JMenuBar createTableMenuBar(){
         final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        tableMenuBar.add(createPreferenceMenu());
         return tableMenuBar;
     }
     private JMenu createFileMenu() {
@@ -83,6 +88,46 @@ public class Table {
         return fileMenu;
     }
 
+    private JMenu createPreferenceMenu() {
+        final JMenu preferenceMenu = new JMenu("Preferences");
+        final JMenuItem flipBoardMenuItem = new JMenuItem("Flip Board");
+        flipBoardMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boardDirection = boardDirection.opposite();
+                boardPanel.drawBoard(chessBoard);
+            }
+        });
+        preferenceMenu.add(flipBoardMenuItem);
+        return preferenceMenu;
+    }
+
+    public enum BoardDirection{
+        NORMAL {
+            @Override
+            List<TilePanel> traverse(final List<TilePanel> boardTiles){
+                return boardTiles;
+            }
+            @Override
+            BoardDirection opposite(){
+                return FLIPPED;
+            }
+        },
+        FLIPPED {
+            @Override
+            List<TilePanel> traverse(final List<TilePanel> boardTiles){
+                return Lists.reverse(boardTiles);
+            }
+
+            @Override
+            BoardDirection opposite() {
+                return null;
+            }
+        };
+        abstract List<TilePanel> traverse(final List<TilePanel> boardTiles);
+        abstract BoardDirection opposite();
+    }
+
     private class BoardPanel extends JPanel {
         final List<TilePanel> boardTiles;
         BoardPanel(){
@@ -98,7 +143,7 @@ public class Table {
         }
         public void drawBoard(final Board board) {
             removeAll();
-            for(final TilePanel tilePanel : boardTiles){
+            for(final TilePanel tilePanel : boardDirection.traverse(boardTiles)){
                 tilePanel.drawTile(board);
                 add(tilePanel);
             }
@@ -135,7 +180,6 @@ public class Table {
                                 sourceTile = null;
                             }
                         }else{
-                            System.out.println("move made");
                             destinationTile = chessBoard.getTile(tileId);
                             final Move move = MoveFactory.createMove(chessBoard, sourceTile.getTileCoordinate(), destinationTile.getTileCoordinate());
                             System.out.println(move.toString());
